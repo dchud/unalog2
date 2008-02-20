caad03cdaf2c4ff280bca282b8526c7b2f616bd0
Dan Chudnov <dchud@umich.edu>
2008-02-19 22:15:25 -0500

    initial import of stub code
    --HG--
    extra : convert_revision : dchud%40umich.edu-20080220031525-hxlggbjbi7fglvt4

---
+++ urls.py
@@ -0,0 +1,9 @@
+from django.conf.urls.defaults import *
+
+urlpatterns = patterns('',
+    # Example:
+    # (r'^unalog2/', include('unalog2.foo.urls')),
+
+    # Uncomment this for admin:
+#     (r'^admin/', include('django.contrib.admin.urls')),
+)
