1fff4cd3150e441893dca90e3ec80e4c04760361
Dan Chudnov <dchud@umich.edu>
2008-07-08 22:27:16 -0400

    Actual home page renders near-correctly
    --HG--
    extra : convert_revision : dchud%40umich.edu-20080709022716-0xalgjf281ukkcta

--- base/views.py
+++ base/views.py
@@ -1,10 +1,12 @@
 from django.http import Http404
 from django.shortcuts import render_to_response
-
+from django.template import RequestContext
 
 from unalog2.base import models as m
 
 
-
 def index (request):
-    return render_to_response("index.html", {'title': "FOO"})
+    qs = m.Entry.objects.order_by('-date_created')
+    qs = qs.exclude(is_private=True)
+    return render_to_response("index.html", {'entries': qs[:50],
+        'title': "FOO"}, RequestContext(request))
