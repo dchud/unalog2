432d4df6eaa768ec4e0f77734b00da65de80f85b
Dan Chudnov <dchud@umich.edu>
2008-08-17 01:41:45 -0400

    Basic pagination working, home page cleaned up a bit
    --HG--
    extra : convert_revision : dchud%40umich.edu-20080817054145-fpptqb39riyspnuj

--- base/views.py
+++ base/views.py
@@ -1,3 +1,4 @@
+from django.core.paginator import Paginator
 from django.http import Http404
 from django.shortcuts import render_to_response
 from django.template import RequestContext
@@ -6,7 +7,17 @@ from unalog2.base import models as m
 
 
 def index (request):
+    context = RequestContext(request)
     qs = m.Entry.objects.order_by('-date_created')
     qs = qs.exclude(is_private=True)
-    return render_to_response("index.html", {'entries': qs[:50],
-        'title': "FOO"}, RequestContext(request))
+    paginator = Paginator(qs, 50)
+    try:
+        page_num = int(request.GET.get('page', '1'))
+        if page_num not in paginator.page_range:
+            raise ValueError, 'Invalid page number'
+    except ValueError:
+        page_num = 1
+    page = paginator.page(page_num)
+    return render_to_response('index.html', {'title': 'FOO', 
+        'paginator': paginator, 'page': page, 
+        }, context)
--- urls.py
+++ urls.py
@@ -1,7 +1,13 @@
 from django.conf.urls.defaults import *
 
+from unalog2.settings import DEBUG, UNALOG_ROOT
+
 urlpatterns = patterns('',
     (r'^$', 'unalog2.base.views.index'),
+
+    # NOTE: don't enable this for real.
+    (r'^s/(?P<path>.*)$', 'django.views.static.serve',
+        {'document_root': "%s/base/media" % UNALOG_ROOT}),
 
     # Uncomment this for admin:
     (r'^admin/', include('django.contrib.admin.urls')),
